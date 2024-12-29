1fc610bbf884281cf77c37a30145a1ab64412050
Sam Benallouch <ossama.web@gmail.com>
2024-12-29 12:40:33 +0100

    added per-user data isolation

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,7 @@ import { useAuthenticator } from "@aws-amplify/ui-react";
 const client = generateClient<Schema>();
 
 export default function App() {
-  const { signOut } = useAuthenticator();
+  const { user, signOut } = useAuthenticator();
 
   const [todos, setTodos] = useState<Array<Schema["Todo"]["type"]>>([]);
 
@@ -32,7 +32,7 @@ export default function App() {
 
   return (
     <main>
-      <h1>My todos</h1>
+      <h1>{user?.signInDetails?.loginId}&apos;s todos</h1>
       <button onClick={createTodo}>+ new</button>
       <ul>
         {todos.map((todo) => (
