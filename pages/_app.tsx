7f38785e245f79e9948bf1fb956e41e6b2fda3d3
Sam Benallouch <ossama.web@gmail.com>
2024-12-30 17:11:45 +0100

    added shadcdn & upgraded nextJS

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,16 +2,24 @@ import "@/styles/app.css";
 import type { AppProps } from "next/app";
 import { Amplify } from "aws-amplify";
 import { Authenticator } from "@aws-amplify/ui-react";
+import { ThemeProvider } from "next-themes";
 import outputs from "@/amplify_outputs.json";
-import "@aws-amplify/ui-react/styles.css";
-import "@cloudscape-design/global-styles/index.css";
+//import "@aws-amplify/ui-react/styles.css";
+//import "@cloudscape-design/global-styles/index.css";
 
 Amplify.configure(outputs);
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Authenticator>
-      <Component {...pageProps} />
-    </Authenticator>
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem={true}
+      disableTransitionOnChange={true}
+    >
+      <Authenticator>
+        <Component {...pageProps} />
+      </Authenticator>
+    </ThemeProvider>
   );
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -2,11 +2,17 @@ import { generateClient } from "aws-amplify/data";
 import type { Schema } from "@/amplify/data/resource";
 import { useAuthenticator } from "@aws-amplify/ui-react";
 import Dashboard from "./dashboard";
+import { ModeToggle } from "@/components/mode-toggle";
 
 const client = generateClient<Schema>();
 
 export default function Todo() {
   const { user, signOut } = useAuthenticator();
-
-  return <Dashboard />;
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <ModeToggle />
+    </div>
+  );
+  //return <Dashboard />;
 }
