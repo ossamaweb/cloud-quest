b8a0678426296abc72a7de8ff795f7a7eb3ffc78
Sam Benallouch <ossama.web@gmail.com>
2024-12-30 17:15:21 +0100

    removed @cloudscape-design

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
 import { generateClient } from "aws-amplify/data";
 import type { Schema } from "@/amplify/data/resource";
 import { useAuthenticator } from "@aws-amplify/ui-react";
-import Dashboard from "./dashboard";
 import { ModeToggle } from "@/components/mode-toggle";
 
 const client = generateClient<Schema>();
@@ -10,9 +9,10 @@ export default function Todo() {
   const { user, signOut } = useAuthenticator();
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <h1 className="text-3xl font-bold underline">
+        Hello {user.signInDetails?.loginId}!
+      </h1>
       <ModeToggle />
     </div>
   );
-  //return <Dashboard />;
 }
