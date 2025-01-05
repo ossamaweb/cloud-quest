f127f09b55e82cba6e9ad190195a47865b81db8e
Sam Benallouch <ossama.web@gmail.com>
2025-01-05 04:32:58 +0100

    improved sidebar left

--- components/sidebar-left.tsx
+++ components/sidebar-left.tsx
@@ -15,51 +15,53 @@ export default function SidebarLeft({ className = "" }: SidebarLeftProps) {
   return (
     <div
       className={cn(
-        "w-64 hidden sm:block bg-background border-r border-muted h-screen p-4",
+        "lg:w-64 hidden sm:block bg-background border-r border-muted h-screen p-4",
         className
       )}
     >
       <nav className="space-y-6">
-        <div className="text-2xl font-bold px-4 py-2 text-[#232F3E]">
-          CloudQuest
+        <div className="text-2xl font-bold px-4 py-2 dark:text-primary text-blue-950 select-none">
+          <span className="hidden lg:inline-block">CloudQuest</span>
+          <span className="lg:hidden inline-block">CQ</span>
         </div>
+
         {/* Add navigation items here */}
         <ul className="space-y-2">
           <li>
             <Button
               active={true}
-              className="w-full text-left py-3 px-4 space-x-4"
+              className="lg:w-full text-left py-3 px-4 space-x-4"
             >
               <HouseIcon />
-              <span>Learn</span>
+              <span className="lg:inline-block hidden">Learn</span>
             </Button>
           </li>
           <li>
             <Button
               active={false}
-              className="w-full text-left py-3 px-4 space-x-4"
+              className="lg:w-full text-left py-3 px-4 space-x-4"
             >
               <UserIcon />
-              <span>Profile</span>
+              <span className="lg:inline-block hidden">Profile</span>
             </Button>
           </li>
           <li>
             <Button
               active={false}
-              className="w-full text-left py-3 px-4 space-x-4"
+              className="lg:w-full text-left py-3 px-4 space-x-4"
             >
               <SettingsIcon />
-              <span>Settings</span>
+              <span className="lg:inline-block hidden">Settings</span>
             </Button>
           </li>
           <li>
             <Button
               onClick={() => signOut()}
               active={false}
-              className="w-full text-left py-3 px-4 space-x-4"
+              className="lg:w-full text-left py-3 px-4 space-x-4"
             >
               <LogOutIcon />
-              <span>Logout</span>
+              <span className="lg:inline-block hidden">Logout</span>
             </Button>
           </li>
         </ul>
