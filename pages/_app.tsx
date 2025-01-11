198113ae4221a6450e1948004ad973eaf738193e
Sam Benallouch <ossama.web@gmail.com>
2025-01-11 23:07:19 +0100

    reset amplify/ui-react/styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,10 +4,8 @@ import { Amplify } from "aws-amplify";
 import { Authenticator } from "@aws-amplify/ui-react";
 import ThemeProvider from "@/components/theme-provider";
 import outputs from "@/amplify_outputs.json";
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
-
-//import "@aws-amplify/ui-react/styles.css";
-//import "@cloudscape-design/global-styles/index.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import "@aws-amplify/ui-react/styles.css";
 
 Amplify.configure(outputs);
 
