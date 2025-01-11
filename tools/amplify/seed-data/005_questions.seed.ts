f646cde4d3d98d88b6a72a31f2a478a9bcbc529b
Sam Benallouch <ossama.web@gmail.com>
2025-01-11 23:06:48 +0100

    fixed questionsSeed types

--- tools/amplify/seed-data/005_questions.seed.ts
+++ tools/amplify/seed-data/005_questions.seed.ts
@@ -4,13 +4,12 @@ import questionsSeed from "@/tools/ai/questions-generator/dist/main.json";
 
 // In the DB model questionData is a string.
 // We stringify the questionData object where we create a record in DB in tools/amplify/seed.ts
-interface QuestionSeed
-  extends Omit<CreateQuestionInput, "questionData" | "difficulty" | "type"> {
+interface QuestionSeed extends Omit<CreateQuestionInput, "questionData"> {
   questionData: QuestionData;
-  difficulty: string;
-  type: string;
 }
 
-const questionsSeedData: Array<QuestionSeed> = [...questionsSeed];
+const questionsSeedData: Array<QuestionSeed> = [
+  ...questionsSeed,
+] as QuestionSeed[];
 
 export default questionsSeedData;
