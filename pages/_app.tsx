4a6dc8cf64d35deb6b9a308f51b4bc931e524dc3
Sam Benallouch <ossama.web@gmail.com>
2025-01-13 07:02:46 +0100

    added inter font

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,16 @@
-import "@/styles/app.css";
 import type { AppProps } from "next/app";
 import { Amplify } from "aws-amplify";
 import { Authenticator } from "@aws-amplify/ui-react";
 import ThemeProvider from "@/components/theme-provider";
 import outputs from "@/amplify_outputs.json";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Inter } from "next/font/google";
 import "@aws-amplify/ui-react/styles.css";
+import "@/styles/app.css";
 
 Amplify.configure(outputs);
-
 const queryClient = new QueryClient();
+const inter = Inter({ subsets: ["latin"] });
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
@@ -21,6 +22,11 @@ export default function App({ Component, pageProps }: AppProps) {
         disableTransitionOnChange={true}
       >
         <Authenticator>
+          <style jsx global>{`
+            html {
+              font-family: ${inter.style.fontFamily};
+            }
+          `}</style>
           <Component {...pageProps} />
         </Authenticator>
       </ThemeProvider>
